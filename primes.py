f37edfe39d86ac88b02f891b1de8157fd0053897
Saskia Lablans <k21009051@kcl.ac.uk>
2022-09-28 09:29:43 +0100

    Provides a list of primes for a request length

--- primes.py
+++ primes.py
@@ -1,6 +1,19 @@
 """List of prime numbers generator."""
-"""ENTER YOUR SOLUTION HERE!"""
 
 def primes(number_of_primes):
     list = []
+    n = 2
+    
+    while len(list) < number_of_primes:
+        prime = True
+
+        for i in range(2,(n//2)+1):
+            if n % i == 0:
+                prime = False
+                break
+        
+        if prime == True:
+            list.append(n)
+        n += 1
+
     return list
